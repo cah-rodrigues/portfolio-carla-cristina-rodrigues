6de00f0814f8d3192b958a0c1867bec46f5d574e
Carla Rodrigues <rodricah1.8@gmail.com>
2025-05-13 17:20:23 -0400

    sidebar mobile ok

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ function openNav() {
     document.getElementsByClassName("sidebar")[0].style.width = "0";
     document.getElementsByClassName("main__document")[0].style.marginLeft= "0";
   }
+
+
+  
